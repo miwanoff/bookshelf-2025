b9399a172a4f085fa9bbc1f67b1b1c3f39de2e70
miwanoff <marina.iva.noffa@gmail.com>
2025-06-15 15:12:03 +0300

    Creare class App

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import booksData from "./books.js";
 
 function Hello() {
   return (
     <div>
-      <h1>Hello, world!</h1>;
+      <h1 style={{ color: "red" }}>Hello, world!</h1>
+      <h2>{booksData[0].name}</h2>
     </div>
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Hello />);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      books: booksData,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.books.map((book) => {
+          // console.log(book.id);
+          return (
+            <div key={book.id}>
+              <p>{book.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+//ReactDOM.createRoot(document.getElementById("root")).render(<Hello />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
